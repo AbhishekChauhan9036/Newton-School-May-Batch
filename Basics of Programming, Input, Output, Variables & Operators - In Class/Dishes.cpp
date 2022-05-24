1d78a097a8ac96457c181a242038e0880776a65f
Abhi <abhishekchauhan9036@gmail.com>
2022-05-24 21:41:45 +0530

    commit message

---
+++ Basics of Programming, Input, Output, Variables & Operators - In Class/Dishes.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+
+	// Your code here
+	int a,b;
+	cin>>a>>b;
+
+	cout<<b-a;
+	return 0;
+}
---
+++ Basics of Programming, Input, Output, Variables & Operators - In Class/Operators.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+
+	// Your code here
+	int a,b;
+	cin>>a>>b;
+
+	cout<<a+b<<endl<<a-b<<endl<<a*b<<endl<<a/b<<endl;
+	return 0;
+}
