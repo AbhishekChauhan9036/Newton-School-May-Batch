899ae28eaefef593eec83894416bca91910c20e1
Abhi <abhishekchauhan9036@gmail.com>
2022-05-24 09:48:43 +0530

    commit message

---
+++ Java Programming - [Syntax] - Post Class/Number-Dilemma.java
@@ -0,0 +1,9 @@
+static int StringToInt(String S){
+//Enter your code here
+return Integer.parseInt(S);
+}
+
+static String IntToString(int N){
+//Enter your code here
+return Integer.toString(N);
+}
