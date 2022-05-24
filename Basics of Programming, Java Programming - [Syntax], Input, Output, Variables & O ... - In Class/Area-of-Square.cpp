b6a0c31b3dbf75cd3a2c3e0bffd3db40231ac818
Abhi <abhishekchauhan9036@gmail.com>
2022-05-24 09:30:40 +0530

    commit message

---
+++ Basics of Programming, Java Programming - [Syntax], Input, Output, Variables & O ... - In Class/Area-of-Square.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+
+	int n;
+	cin>>n;
+
+	cout<<n*n;
+	return 0;
+}
---
+++ Basics of Programming, Java Programming - [Syntax], Input, Output, Variables & O ... - In Class/Ram-Grades.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> // header file includes every Standard library
+using namespace std;
+
+int main() {
+
+	int a,b,c,d;
+
+	cin>>a>>b>>c>>d;
+
+	int ans=(a+b+c+d)/4;
+
+	cout<<ans;
+	return 0;
+}
