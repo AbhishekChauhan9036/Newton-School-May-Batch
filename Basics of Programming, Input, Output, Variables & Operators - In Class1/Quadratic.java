46a8bcade819f7c89e46e844a8e586d7ea3c4239
Abhi <abhishekchauhan9036@gmail.com>
2022-06-01 06:50:45 +0530

    commit message

---
+++ Basics of Programming, Input, Output, Variables & Operators - In Class1/Quadratic.java
@@ -0,0 +1,18 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                    Scanner in=new Scanner(System.in);
+					int a=in.nextInt();
+					int b=in.nextInt();
+					int c=in.nextInt();
+					int x=in.nextInt();
+					int z=a*x*x+b*x+c;
+					System.out.print(z);
+
+					  
+	}
+}
